8aa1612083a44b9b80772d29b05e41669e7fb724
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-22 13:47:55 +0300

    my commit edit code

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,13 +1,13 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem: ElementRef) {
-    this.elem.nativeElement.style.color = 'gold';
-    this.elem.nativeElement.style.fontsize = '26px';
-   }
+  constructor(private elem:ElementRef) {}
+  @HostListener("click") onClicks(){
+    this.elem.nativeElement.style.color='red';
+  }
 
 }
