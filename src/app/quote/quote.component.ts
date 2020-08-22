47f04e98e1f1a60b2a807594730ce1a9ffab2f6f
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-22 15:23:01 +0300

    my commit add images

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   
   quotes:Quote[] = [
-    new Quote(1,'The man that hath no music in himself, Nor is not moved with concord of sweet sounds, is fit for treasons, stratagems and spoils.','William Shakespeare',"Nyururu",new Date(2020,8,22),0,0),
-    new Quote(2,'Must is a hard nut to crack, but it has a sweet kernel.','Charles Spurgeon', "Nyururu",new Date(2020,8,22),0,0),
-    new Quote(3,'As a man sow, shall he reap. and I know that talk is cheap. But the heat of the battle is as sweet as the victory.' , 'Bob Marley', "Nyururu",new Date(2020,8,22),0,0),
-    new Quote(4,'Yesterday is not ours to recover, but tomorrow is ours to win or lose.' , 'Lyndon B. Johnson', "Nyururu",new Date(2020,8,22),0,0),
-    new Quote(5,'Virtually nothing is impossible in this world if you just put your mind to it and maintain a positive attitude.', 'Lou Holtz', "Nyururu",new Date(2020,8,22),0,0),
+    new Quote(1, 'The man that hath no music in himself, Nor is not moved with concord of sweet sounds, is fit for treasons, stratagems and spoils.','William Shakespeare',"Nyururu",new Date(2020,8,22),0,0),
+    new Quote(2, 'Must is a hard nut to crack, but it has a sweet kernel.','Charles Spurgeon', "Nyururu",new Date(2020,8,22),0,0),
+    new Quote(3, 'As a man sow, shall he reap. and I know that talk is cheap. But the heat of the battle is as sweet as the victory.' , 'Bob Marley', "Nyururu",new Date(2020,8,22),0,0),
+    new Quote(4, 'Yesterday is not ours to recover, but tomorrow is ours to win or lose.' , 'Lyndon B. Johnson', "Nyururu",new Date(2020,8,22),0,0),
+    new Quote(5, 'Virtually nothing is impossible in this world if you just put your mind to it and maintain a positive attitude.', 'Lou Holtz', "Nyururu",new Date(2020,8,22),0,0),
   ];
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
