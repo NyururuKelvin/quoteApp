247486516542316d4a7588e9d19a7d884a8fed6f
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-22 11:37:20 +0300

    my commit edit details

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-quote-details',
+  selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
   styleUrls: ['./quote-detail.component.css']
 })
